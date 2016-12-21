d64a73ddb61df26062b40109ead29feacd84aefe
Alberto <alberto91base@gmail.com>
2016-12-21 11:36:38 +0100

    Accordion con accodion dentro.

--- app/app.js
+++ app/app.js
@@ -3,16 +3,17 @@ var app = {};
 jQuery( document ).ready(function(){
 	app.main = (function () {
 		var settings = {
-				target: '.js-accordion',
-				accordionList: '.js-accordions-list',
-				accordionListItem: '.js-accordionlist-item',
-				accordionListLink: '.js-accordionlist-link',
+				target: 						'.js-accordion',
+				accordionList: 			'.js-accordions-list',
+				accordionListItem: 	'.js-accordionlist-item',
+				accordionListLink: 	'.js-accordionlist-link',
 				accordionPanelItem: '.js-accordions-panel-item',
-				active: 'active',
-				classAccordion: 'accordionItems_',
+				active: 						'active',
+				classAccordion: 		'accordionItems_',
 				contadorAccordions: 1,
-				itemActivate: 'itemActivate',
-				expanded: false
+				itemActivate: 			'itemActivate',
+				itemsWorking: 			'itemsWorkingAccordion',
+				expanded: 					false
 		},
 		init = function(){
 			accordion();
@@ -28,24 +29,26 @@ jQuery( document ).ready(function(){
 		},
 
 		accordionItem = function($elem){
-			var $elementsLinks = $elem.find(settings.accordionList + ' ' + settings.accordionListLink),
-				$accordionLink = $elem.find(settings.accordionListLink),
-				$accordionList = $elem.find(settings.accordionListItem);
+			var $elementsLinks = selectedElementsWorkings($elem, settings.accordionListLink);
 
 			$elementsLinks.on('click', function(event){
 				event.preventDefault();
-				var $this 			= $(this),
-					$parent 		= $elem,
-					$accordionListItems 	= $parent.find( settings.accordionListItem);
+				var $this 						= $(this),
+					$parent 						= $this.parents(settings.target + ":first"),
+					$accordionListItems = $parent.find( settings.accordionListItem);
+
+				markElementsWorkings($parent, settings.accordionListItem);
+				markElementsWorkings($parent, settings.accordionListLink);
+				markElementsWorkings($parent, settings.accordionPanelItem);
 
 				//busco todos los accordions y los reseteo
 				if( $accordionListItems.length > 0 ){
-					$accordionListItems.find(settings.accordionListLink)
+					$accordionListItems.find(settings.accordionListLink + "." + settings.itemsWorking)
 						.attr('tabindex', '-1')
 						.attr('aria-selected', 'false');
 				}
 
-				var $parentActivate = $this.parents( settings.accordionListItem );
+				var $parentActivate = $this.parents( settings.accordionListItem + ":first" );
 				var $panelActivate = $('#' + $this.attr('aria-controls'));
 				//si el elemento clicado esta activo
 				if( $parentActivate.hasClass( settings.active ) ){
@@ -57,12 +60,13 @@ jQuery( document ).ready(function(){
 							.attr('tabindex', '0')
 							.attr('aria-selected', 'true')
 							.attr('aria-expanded','false');
+						deleteElementsWorkings();
 					});
 
 				}else{//si el elemento clicado no esta activo
 					$panelActivate.addClass(settings.itemActivate);
 					//cerrar los otros paneles;
-					var $accordionPanelItems = $parent.find( settings.accordionPanelItem + ':not(.'+settings.itemActivate+')' );
+					var $accordionPanelItems = $parent.find( settings.accordionPanelItem + ':not(.'+settings.itemActivate+')' + "." + settings.itemsWorking );
 					$accordionListItems
 							.removeClass( settings.active );
 					$accordionPanelItems.slideUp( function(){
@@ -70,10 +74,11 @@ jQuery( document ).ready(function(){
 							.attr('aria-hidden','true')
 							.removeClass( settings.active );
 
-						$accordionListItems.find( settings.accordionListLink )
+						$accordionListItems.find( settings.accordionListLink + "." + settings.itemsWorking)
 							.attr('tabindex', '-1')
 							.attr('aria-selected', 'false')
 							.attr('aria-expanded','false');
+						deleteElementsWorkings();
 					});
 					//activamos el elemento nuevo
 					$parentActivate
@@ -91,35 +96,51 @@ jQuery( document ).ready(function(){
 							.addClass( settings.active);
 
 						$panelActivate.removeClass(settings.itemActivate);
+						deleteElementsWorkings();
 					});
 				}
+				deleteElementsWorkings();
 			});
 
-			$accordionLink.on('keydown', function(e) {
-	      var $that 			= jQuery(this),
-	      	$parent 		= $that.parents(settings.target),
-	        $accordionAgrup = $that.parents(settings.accordionListItem),
-	        elementsAccordion 	= $elem.find(settings.accordionListItem).length - 1,
-	        currentAccordion 		= $accordionAgrup.index(),
-	        $element;
+			$elementsLinks.on('keydown', function(e) {
+				var $that 					 = jQuery(this),
+					$parent 					 = $that.parents(settings.target + ":first"),
+					$accordionAgrup 	 = $that.parents(settings.accordionListItem + ":first"),
+					currentAccordion 	 = $accordionAgrup.index(),
+					$element;
 
-	      if( $parent.attr('data-expanded') === "true" ){
+				//marcamos los elementos en los que estamos trabajando
+				markElementsWorkings($parent, settings.accordionListItem);
+				markElementsWorkings($parent, settings.accordionListLink);
+
+				var $elementsAccordion = $parent.find(settings.accordionListItem + "." + settings.itemsWorking),
+				elementsAccordion  = $elementsAccordion.length - 1;
+
+				if( $parent.attr('data-expanded') === "true" ){
 					settings.expanded = true;
 				}
-	      if (!(e.shiftKey && e.keyCode === 9)) {
+				if (!(e.shiftKey && e.keyCode === 9)) {
 					if (!(e.keyCode === 9)) { //accordion
 						//e.preventDefault();
 						//key left or key up - previous accordion
 						if ((e.keyCode === 37 || e.keyCode === 38)) {
 							e.preventDefault();
-							$accordionList.eq(currentAccordion).find(settings.accordionListLink).attr('tabindex', '-1').attr('aria-selected','false');
+							$elementsAccordion.eq(currentAccordion).find(settings.accordionListLink + "." + settings.itemsWorking)
+								.attr('tabindex', '-1')
+								.attr('aria-selected','false');
 							if (currentAccordion <= elementsAccordion) {
 								//there is elements-accordion on left
-								$element = $accordionList.eq(currentAccordion - 1).find(settings.accordionListLink).focus().attr('tabindex', '0').attr('aria-selected','true');
+								$element = $elementsAccordion.eq(currentAccordion - 1).find(settings.accordionListLink + "." + settings.itemsWorking)
+									.focus()
+									.attr('tabindex', '0')
+									.attr('aria-selected','true');
 							} else {
 								if (currentAccordion == 0) {
 									//start on last accordion again
-									$element = $accordionList.eq(elementsAccordion).find(settings.accordionListLink).focus().attr('tabindex', '0').attr('aria-selected','true');
+									$element = $elementsAccordion.eq(elementsAccordion).find(settings.accordionListLink + "." + settings.itemsWorking)
+										.focus()
+										.attr('tabindex', '0')
+										.attr('aria-selected','true');
 								}
 							}
 							if(settings.expanded){
@@ -130,14 +151,22 @@ jQuery( document ).ready(function(){
 							//key right or key down - next accordion
 							if ((e.keyCode === 39 || e.keyCode === 40)) {
 								e.preventDefault();
-								$accordionList.eq(currentAccordion).find(settings.accordionListLink).attr('tabindex', '-1').attr('aria-selected','false');
+								$elementsAccordion.eq(currentAccordion).find(settings.accordionListLink + "." + settings.itemsWorking)
+									.attr('tabindex', '-1')
+									.attr('aria-selected', 'false');
 								if (currentAccordion < elementsAccordion) {
 									//there is elements-accordion on right
-									$element = $accordionList.eq(currentAccordion + 1).find(settings.accordionListLink).focus().attr('tabindex', '0').attr('aria-selected','true');
+									$element = $elementsAccordion.eq(currentAccordion + 1).find(settings.accordionListLink + "." + settings.itemsWorking)
+										.focus()
+										.attr('tabindex', '0')
+										.attr('aria-selected', 'true');
 								} else {
 									if (currentAccordion == elementsAccordion) {
 										//start on first accordion again
-										$element = $accordionList.eq(0).find(settings.accordionListLink).focus().attr('tabindex', '0').attr('aria-selected','true');
+										$element = $elementsAccordion.eq(0).find(settings.accordionListLink + "." + settings.itemsWorking)
+											.focus()
+											.attr('tabindex', '0')
+											.attr('aria-selected', 'true');
 									}
 								}
 								if(settings.expanded){
@@ -147,7 +176,7 @@ jQuery( document ).ready(function(){
 						}
 					}
 				}
-	    });
+			});
 
 			accordionItemAtributes($elem, $elementsLinks);
 		},
@@ -194,6 +223,23 @@ jQuery( document ).ready(function(){
 				.attr('aria-hidden','false')
 				.addClass( settings.active )
 				.slideDown();
+		},
+		//selecciona los elemenos en los que estamos trabajando le añade una clase y devuelve los elementos que necesitamos
+		selectedElementsWorkings = function($parentElements, elementsSearch){
+			var $elem = $parentElements.find(elementsSearch);
+			$elem.addClass(settings.itemsWorking);
+			$parentElements.find(settings.target + " ." + settings.itemsWorking).removeClass(settings.itemsWorking);
+			$elem = $(elementsSearch + "." + settings.itemsWorking);
+			$("." + settings.itemsWorking).removeClass(settings.itemsWorking);
+			return $elem;
+		},
+		markElementsWorkings = function($elem, elementoMarcar){
+			$elem.find(elementoMarcar).addClass(settings.itemsWorking);
+			$elem.find(settings.target + " ." + settings.itemsWorking).removeClass(settings.itemsWorking);
+		},
+		//eliminamos elementos workings
+		deleteElementsWorkings = function(){
+			$("." + settings.itemsWorking).removeClass(settings.itemsWorking);
 		};
 
 		// Public API
